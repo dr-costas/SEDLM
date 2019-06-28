00d7108caf7f39510a5a910cd0c1b8b04abedbb2
drossos <kostas.drosos@icloud.com>
2019-06-28 16:57:44 +0300

    Added CRNN module.

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from . import crnn
+
+__author__ = 'Konstantinos Drossos -- Tampere University'
+__docformat__ = 'reStructuredText'
+__all__ = ['crnn']
+
+# EOF
