2ecc8ce62ef884e06e3f16dd6a3e91f5638e6ad9
drossos <kostas.drosos@icloud.com>
2019-07-01 14:04:34 +0300

    Refactored imports.

--- tools/__init__.py
+++ tools/__init__.py
@@ -7,7 +7,6 @@ import tools._feature_extraction as feature_extraction
 import tools._file_io as file_io
 import tools._metrics as metrics
 import tools._modules_functions as modules_functions
-import tools._experiments_processes as experiments_processes
 import tools._argument_parsing as argument_parsing
 
 __author__ = 'Konstantinos Drossos -- Tampere University'
@@ -15,7 +14,7 @@ __docformat__ = 'reStructuredText'
 __all__ = [
     'printing', 'content', 'feature_extraction',
     'file_io', 'metrics', 'modules_functions',
-    'experiments_processes', 'argument_parsing'
+    'argument_parsing'
 ]
 
 # EOF
