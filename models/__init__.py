26bc25dc2d2b3ef9364ca52724009912564e50af
drossos <kostas.drosos@icloud.com>
2019-07-01 16:32:43 +0300

    Fixed init.

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from . import crnn
+from .crnn import CRNN
+from .tf_crnn import TFCRNN
 
 __author__ = 'Konstantinos Drossos -- Tampere University'
 __docformat__ = 'reStructuredText'
-__all__ = ['crnn']
+__all__ = ['CRNN', 'TFCRNN']
 
 # EOF
