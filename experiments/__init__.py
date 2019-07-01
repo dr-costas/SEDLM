d5c8292deb064e89387c6f569f2f8a90e43dc514
drossos <kostas.drosos@icloud.com>
2019-07-01 14:42:15 +0300

    Init file for the package with the processes for the experiments.

---
+++ experiments/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from experiments import baseline
+from experiments import tf_experiment
+
+__author__ = 'Konstantinos Drossos -- Tampere University'
+__docformat__ = 'reStructuredText'
+__all__ = ['baseline', 'tf_experiment']
+
+
+# EOF
