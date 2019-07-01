da48d39a3dda4ab5133c61b05f4ea7f67934555e
drossos <kostas.drosos@icloud.com>
2019-07-01 14:45:01 +0300

    Refactored the imports.

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import tools._printing as printing
-import tools._content as content
-import tools._feature_extraction as feature_extraction
-import tools._file_io as file_io
-import tools._metrics as metrics
-import tools._modules_functions as modules_functions
-import tools._argument_parsing as argument_parsing
+from tools import printing, file_io, metrics,\
+    argument_parsing, various
 
 __author__ = 'Konstantinos Drossos -- Tampere University'
 __docformat__ = 'reStructuredText'
 __all__ = [
-    'printing', 'content', 'feature_extraction',
-    'file_io', 'metrics', 'modules_functions',
-    'argument_parsing'
+    'printing', 'file_io', 'metrics',
+    'argument_parsing', 'various'
 ]
 
 # EOF
