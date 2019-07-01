f21ae02c6cfeb77ba3d88ea5832ee8dcc55e8a19
drossos <kostas.drosos@icloud.com>
2019-07-01 15:16:26 +0300

    Narrowed imports.

--- experiments/_processes.py
+++ experiments/_processes.py
@@ -7,9 +7,12 @@ from copy import deepcopy
 from torch import no_grad, cat, zeros
 from torch.optim import Adam
 from torch.nn import BCEWithLogitsLoss, utils
-from torch import cuda
+from torch.cuda import is_available
 
-from tools import metrics, printing
+from tools.metrics import f1_per_frame, error_rate_per_frame
+from tools.printing import print_msg, inform_about_device, \
+    InformAboutProcess, print_evaluation_results, \
+    print_training_results
 from data_feeders import get_tut_sed_data_loader
 
 __author__ = 'Konstantinos Drossos -- Tampere University'
@@ -99,7 +102,7 @@ def testing(model, data_loader, f1_func, er_func, device):
     f1_score = f1_func(hat_values, true_values).mean()
     er_score = er_func(hat_values, true_values).mean()
 
-    printing.print_evaluation_results(f1_score, er_score, end_time)
+    print_evaluation_results(f1_score, er_score, end_time)
 
 
 def training(model, data_loader_training, optimizer, objective, f1_func, er_func,
@@ -179,7 +182,7 @@ def training(model, data_loader_training, optimizer, objective, f1_func, er_func
 
             end_time = time() - start_time
 
-            printing.print_training_results(
+            print_training_results(
                 epoch=epoch, training_loss=epoch_tr_loss,
                 validation_loss=epoch_va_loss,
                 training_f1=f1_score_training,
@@ -190,11 +193,10 @@ def training(model, data_loader_training, optimizer, objective, f1_func, er_func
             )
 
             if epochs_waiting >= validation_patience:
-                printing.print_msg('', start='')
-                printing.print_msg(
+                print_msg(
                     'Early stopping! Lowest validation loss: {:7.3f} at epoch: {:3d}'.format(
                         biggest_epoch_loss, best_model_epoch
-                    ), end='\n\n')
+                    ), start='\n-- ', end='\n\n')
                 break
 
         if best_model is not None:
@@ -206,7 +208,7 @@ def training(model, data_loader_training, optimizer, objective, f1_func, er_func
         if best_model is not None:
             model.load_state_dict(best_model)
 
-        printing.print_msg('Keyboard stopping, proceeding to testing', start='\n\n-- ')
+        print_msg('Keyboard stopping, proceeding to testing', start='\n\n-- ')
         return model
 
 
@@ -218,83 +220,86 @@ def experiment(settings, model_class):
     :param model_class: The class of the model.
     :type model_class: callable
     """
-    device = 'cuda' if cuda.is_available() else 'cpu'
-    printing.inform_about_device(device)
+    device = 'cuda' if is_available() else 'cpu'
+    inform_about_device(device)
 
-    with printing.InformAboutProcess('Creating the model'):
+    with InformAboutProcess('Creating the model'):
         model = model_class(**settings['sed_model'])
+        model = model.to(device)
 
-    model = model.to(device)
-
-    with printing.InformAboutProcess('Creating training data loader'):
+    with InformAboutProcess('Creating training data loader'):
         training_data = get_tut_sed_data_loader(
-            dataset_split='training', **settings['data_loader'])
+            split='training', **settings['data_loader'])
 
-    with printing.InformAboutProcess('Creating validation data loader'):
+    with InformAboutProcess('Creating validation data loader'):
         validation_data = get_tut_sed_data_loader(
-            dataset_split='validation', **settings['data_loader'])
+            split='validation', **settings['data_loader'])
 
-    with printing.InformAboutProcess('Creating optimizer'):
+    with InformAboutProcess('Creating optimizer'):
         optimizer = Adam(model.parameters(), lr=settings['optimizer']['lr'])
 
-    with printing.InformAboutProcess('Setting the teacher forcing attributes'):
-        model.apply_after = settings['tf']['apply_after_epochs'] * len(training_data)
-        model.gamma_factor = settings['tf']['gamma_factor']
-        model.mul_factor = settings['tf']['mul_factor']
-        model.min_prob = settings['tf']['min_prob']
-        model.max_prob = settings['tf']['max_prob']
-        model.batch_counter = len(training_data)
+    if hasattr(model, 'scheduled_sampling'):
+        with InformAboutProcess('Setting the teacher forcing attributes', end='\n\n'):
+            model.apply_after = settings['tf']['apply_after_epochs'] * len(training_data)
+            model.gamma_factor = settings['tf']['gamma_factor']
+            model.mul_factor = settings['tf']['mul_factor']
+            model.min_prob = settings['tf']['min_prob']
+            model.max_prob = settings['tf']['max_prob']
+            model.batch_counter = len(training_data)
+    else:
+        print_msg('', start='')
 
-    printing.print_msg('', start='')
+    common_kwargs = {
+        'f1_func': f1_per_frame,
+        'er_func': error_rate_per_frame,
+        'device': device
+    }
 
     len_2 = max([len('Training examples/batches'), len('Validation examples/batches')])
 
-    printing.print_msg('{m2:<{len_2}}: {d1:5d} /{d2:5d}'.format(
+    print_msg('{m2:<{len_2}}: {d1:5d} /{d2:5d}'.format(
         m2='Training examples/batches',
         d1=len(training_data) * settings['data_loader']['batch_size'],
         d2=len(training_data),
         len_2=len_2
     ))
 
-    printing.print_msg('{m2:<{len_2}}: {d1:5d} /{d2:5d}'.format(
+    print_msg('{m2:<{len_2}}: {d1:5d} /{d2:5d}'.format(
         m2='Validation examples/batches',
         d1=len(validation_data) * settings['data_loader']['batch_size'],
         d2=len(validation_data),
         len_2=len_2
     ), end='\n\n')
 
-    printing.print_msg('Starting training', start='\n\n-- ', end='\n\n')
+    print_msg('Starting training', start='\n\n-- ', end='\n\n')
 
     optimized_model = training(
         model=model, data_loader_training=training_data,
         optimizer=optimizer, objective=BCEWithLogitsLoss(),
-        f1_func=metrics.f1_per_frame, er_func=metrics.error_rate_per_frame,
         epochs=settings['training']['epochs'],
         data_loader_validation=validation_data,
         validation_patience=settings['training']['validation_patience'],
-        device=device, grad_norm=settings['training']['grad_norm']
+        grad_norm=settings['training']['grad_norm'], **common_kwargs
     )
 
     del training_data
 
-    printing.print_msg('Starting testing', start='\n\n-- ', end='\n\n')
+    print_msg('Starting testing', start='\n\n-- ', end='\n\n')
 
     if settings['data_loader']['data_version'] == 'synthetic':
         del validation_data
-        with printing.InformAboutProcess('Creating testing data loader'):
+        with InformAboutProcess('Creating testing data loader'):
             testing_data = get_tut_sed_data_loader(
-                dataset_split='testing', **settings['data_loader'])
+                split='testing', **settings['data_loader'])
     else:
-        printing.print_msg('Using X-fold setting.', start='\n\n-- ')
+        print_msg('Using X-fold setting.', start='\n\n-- ')
         testing_data = validation_data
 
     testing(
         model=optimized_model, data_loader=testing_data,
-        f1_func=metrics.f1_per_frame,
-        er_func=metrics.error_rate_per_frame,
-        device=device
+        **common_kwargs
     )
 
-    printing.print_msg('That\'s all!', start='\n\n-- ', end='\n\n')
+    print_msg('That\'s all!', start='\n\n-- ', end='\n\n')
 
 # EOF
