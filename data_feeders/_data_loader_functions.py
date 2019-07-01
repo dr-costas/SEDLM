994a49f4addb6ca0c13cf4aba9624ce398c16c5b
drossos <kostas.drosos@icloud.com>
2019-07-01 12:22:57 +0300

    Data loader functions.

--- data_feeders/_data_loader_functions.py
+++ data_feeders/_data_loader_functions.py
@@ -2,19 +2,21 @@
 # -*- coding: utf-8 -*-
 
 from torch.utils.data import DataLoader
+
 from ._tut_sed_synthetic_2016 import TUTSEDSynthetic2016
 from ._tut_sed_real_life_2017 import TUTSEDRealLife2017
 from ._tut_sed_real_life_2016 import TUTSEDRealLife2016
 
-__author__ = 'Konstantinos Drossos'
+__author__ = 'Konstantinos Drossos -- Tampere University'
 __docformat__ = 'reStructuredText'
 __all__ = ['get_tut_sed_data_loader']
 
 
 def get_tut_sed_data_loader(dataset_root_dir, dataset_split,
                             batch_size, shuffle, drop_last,
-                            tutsed_features, normalized_features=True,
-                            data_version='synthetic', data_fold=None, scene=None):
+                            normalized_features=True,
+                            data_version='synthetic',
+                            data_fold=None, scene=None):
     """Creates and returns the data loader.
 
     :param dataset_root_dir: The root dir for the dataset.
@@ -28,8 +30,6 @@ def get_tut_sed_data_loader(dataset_root_dir, dataset_split,
     :type shuffle: bool
     :param drop_last: Drop last examples?
     :type drop_last: bool
-    :param tutsed_features: Use features extracted from TUTSED code?
-    :type tutsed_features: bool
     :param normalized_features: Use SMUV features?
     :type normalized_features: bool
     :param data_version: Which version of the dataset? Accepted\
@@ -42,36 +42,25 @@ def get_tut_sed_data_loader(dataset_root_dir, dataset_split,
     :return: The TUT BREACNNModel data loader.
     :rtype: torch.utils.data.DataLoader
     """
+    common_kwargs = {
+        'root_dir': dataset_root_dir,
+        'split': dataset_split,
+        'norm_features': normalized_features}
+
     if data_version == 'synthetic':
-        dataset = TUTSEDSynthetic2016(
-            dataset_root_dir=dataset_root_dir,
-            dataset_split=dataset_split,
-            tutsed_features=tutsed_features,
-            normalized_features=normalized_features,
-            data_version=data_version
-        )
+        dataset = TUTSEDSynthetic2016(**common_kwargs)
     else:
         if data_version == 2016:
             dataset = TUTSEDRealLife2016(
-                dataset_root_dir=dataset_root_dir,
-                dataset_split=dataset_split,
                 data_fold=data_fold, scene=scene,
-                normalized_features=normalized_features,
-                data_version=data_version
-            )
+                **common_kwargs)
         else:
             dataset = TUTSEDRealLife2017(
-                dataset_root_dir=dataset_root_dir,
-                dataset_split=dataset_split,
-                data_fold=data_fold,
-                normalized_features=normalized_features,
-                data_version=data_version
-            )
+                data_fold=data_fold, **common_kwargs)
 
     return DataLoader(
         dataset=dataset, batch_size=batch_size,
         shuffle=shuffle if dataset_split == 'training' else False,
-        drop_last=drop_last
-    )
+        drop_last=drop_last)
 
 # EOF
