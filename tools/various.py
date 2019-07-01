d1f508746cc996ea0a494eb7ce8379677d326869
drossos <kostas.drosos@icloud.com>
2019-07-01 14:45:56 +0300

    Moved the argument parsing in this file.

--- tools/various.py
+++ tools/various.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# imports
+from argparse import ArgumentParser
 
 __author__ = 'Konstantinos Drossos -- Tampere University'
 __docformat__ = 'reStructuredText'
-__all__ = ['CheckAllNone']
+__all__ = ['CheckAllNone', 'get_argument_parser']
 
 
 class CheckAllNone(object):
@@ -28,5 +28,18 @@ class CheckAllNone(object):
     def __call__(self, fn):
         self.fn = fn
         return self._decorated
+
+
+def get_argument_parser():
+    """Creates and returns the ArgumentParser for this project.
+
+    :return: The argument parser.
+    :rtype: argparse.ArgumentParser
+    """
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument('--config-file', type=str, default='')
+    arg_parser.add_argument('--baseline', type=bool, default=False)
+
+    return arg_parser
 
 # EOF
