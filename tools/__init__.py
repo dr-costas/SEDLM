eb35e6f2ba00c968ceecf3e556b3cbac304b6a60
drossos <kostas.drosos@icloud.com>
2019-07-01 14:55:38 +0300

    Refactored imports.

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from tools import printing, file_io, metrics,\
-    argument_parsing, various
+from tools import printing, file_io, metrics, various
 
 __author__ = 'Konstantinos Drossos -- Tampere University'
 __docformat__ = 'reStructuredText'
 __all__ = [
-    'printing', 'file_io', 'metrics',
-    'argument_parsing', 'various'
+    'printing', 'file_io', 'metrics', 'various'
 ]
 
 # EOF
