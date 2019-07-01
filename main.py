8fafbee9adb24eed34a6ccc1e573913af9706072
drossos <kostas.drosos@icloud.com>
2019-07-01 14:40:40 +0300

    Main entry point for the project.

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tools.printing import print_date_and_time, print_yaml_settings
+from tools.argument_parsing import get_argument_parser
+from tools.file_io import load_settings_file
+from experiments.tf_expriment import do_process as tf_process
+from experiments.baseline import do_process as baseline
+
+__author__ = 'Konstantinos Drossos -- Tampere University'
+__docformat__ = 'reStructuredText'
+__all__ = []
+
+
+def main():
+    """Main entry point for the project.
+    """
+    print_date_and_time()
+
+    arg_parser = get_argument_parser()
+    args = arg_parser.parse_args()
+
+    experiment_process = baseline if args.baseline else tf_process
+
+    settings = load_settings_file(args.config_file)
+    print_yaml_settings(settings)
+
+    experiment_process(settings=settings)
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
